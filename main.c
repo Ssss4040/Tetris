a1126d0505f757c327fdd9ee34ba2fa9f1983112
ssss4040 <sburygin52@bk.ru>
2022-08-25 00:05:39 +0700

    добавлены фигуры

--- main.c
+++ main.c
@@ -5,25 +5,26 @@
 #include <time.h>
 #include <stdio.h>
 
-#define WIDTH 80
-#define LENGTH 50
+#define WIDTH 40
+#define LENGTH 40
 #define TOP 0
-#define BOTTOM 49
+// #define BOTTOM 49
 #define LEFT 0
-#define RIGHT 79
+// #define RIGHT 79
 
 void start_field(int *field);
-void figures(int *field);
+void sample_figures(int *field, int *list_figures);
+void figures_field(int *field, int *list_figures);
 int check_update_filed(int *field, int *last_field);
 void output_field(int *field);
 void pausedelay(int milli_seconds);
 
 int main() {
     int field[LENGTH][WIDTH];
-    int x = 0;
+    int x = 0, list_figures = 0;
     start_field(&field[0][0]);
     while (x != 100) {
-        figures(&field[0][0]);
+        figures_field(&field[0][0], &list_figures);
         output_field(&field[0][0]);
     }   
     return 0;
@@ -33,28 +34,85 @@ void start_field(int *field) {
     
     for (int i = 0; i < LENGTH; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            if (i == TOP || j == LEFT || i == BOTTOM  || j ==RIGHT)
+            if (i == TOP || j == LEFT || i == (WIDTH-1)  || j == (LENGTH-1))
                 field[i*WIDTH+j] = 1;
             else field[i*WIDTH+j] = 0;
         }
     }
 }
 
-void figures(int *field) {
-    int last_filed[LENGTH][WIDTH];
-    for (int i = LENGTH-1; i >= 0; i--) { // 0 .. 49
-        for (int j = WIDTH-1; j >= 0; j--) {
-            last_filed[i][j] == field[i*WIDTH+j];
-        }
+ void sample_figures(int *field, int *list_figures) {
+    switch (*list_figures) {
+        case 0: {
+            field[1*WIDTH+(WIDTH/2-1)] = 2; // ##
+            field[1*WIDTH+(WIDTH/2-2)] = 2; // ##
+            field[2*WIDTH+(WIDTH/2-1)] = 2;
+            field[2*WIDTH+(WIDTH/2-2)] = 2;
+            (*list_figures)++;
+            } break;
+        case 1: {
+            field[1*WIDTH+(WIDTH/2-1)] = 2; //  #
+            field[2*WIDTH+(WIDTH/2-1)] = 2; //  #
+            field[3*WIDTH+(WIDTH/2-1)] = 2; // ##
+            field[3*WIDTH+(WIDTH/2-2)] = 2;
+            (*list_figures)++;
+        }break;
+        case 2: {
+            field[1*WIDTH+(WIDTH/2-1)] = 2; // ##
+            field[1*WIDTH+(WIDTH/2)] = 2;   //  ##
+            field[2*WIDTH+(WIDTH/2)] = 2;
+            field[2*WIDTH+(WIDTH/2+1)] = 2;
+            (*list_figures)++;
+        }break;
+        case 3: {
+            field[1*WIDTH+(WIDTH/2)] = 2;   //  #
+            field[2*WIDTH+(WIDTH/2-1)] = 2; // ###
+            field[2*WIDTH+(WIDTH/2)] = 2;
+            field[2*WIDTH+(WIDTH/2+1)] = 2;
+            (*list_figures)++;
+        }break;
+        case 4: {
+            field[1*WIDTH+(WIDTH/2)] = 2;   //  ##
+            field[1*WIDTH+(WIDTH/2+1)] = 2; // ##
+            field[2*WIDTH+(WIDTH/2)] = 2;
+            field[2*WIDTH+(WIDTH/2-1)] = 2;
+            (*list_figures)++;
+        }break;
+        case 5: {
+            field[1*WIDTH+(WIDTH/2)] = 2;   // # 
+            field[2*WIDTH+(WIDTH/2)] = 2;   // #
+            field[3*WIDTH+(WIDTH/2)] = 2;   // ##
+            field[3*WIDTH+(WIDTH/2+1)] = 2;
+            (*list_figures)++;
+        }break;
+        case 6: {
+            field[1*WIDTH+(WIDTH/2)] = 2;   // # 
+            field[2*WIDTH+(WIDTH/2)] = 2;   // #
+            field[3*WIDTH+(WIDTH/2)] = 2;   // #
+            field[4*WIDTH+(WIDTH/2)] = 2;   // #
+            *list_figures = 0;
+        }break;
+        
+        
+        
+        default: break;
     }
-    field[1*WIDTH+13] = 2;
-    field[1*WIDTH+14] = 2;
-    field[2*WIDTH+13] = 2;
-    field[2*WIDTH+14] = 2;
+ }
+
+void figures_field(int *field, int *list_figures) {
+    int last_filed[LENGTH][WIDTH];
+
+    sample_figures(field, list_figures);
+    
     
     output_field(field);
     while (check_update_filed(field, &last_filed[0][0]) != 0) {
         int move = 0, from = 0, before = 0, changes = 0;
+        for (int i = LENGTH-1; i >= 0; i--) { // 0 .. 49
+            for (int j = WIDTH-1; j >= 0; j--) {// 0 .. 79
+                last_filed[i][j] = field[i*WIDTH+j];
+            }
+        }
         switch (getchar()) {
             case 'a': {
                 move--;
@@ -64,7 +122,7 @@ void figures(int *field) {
                             field[i*WIDTH+j] = 0;
                             field[(i+1)*WIDTH+(j+move)] = 2;
                             output_field(field);
-                            pausedelay(150000);
+                            //pausedelay(150000);
                         }
                     }
                 }
@@ -77,7 +135,7 @@ void figures(int *field) {
                             field[i*WIDTH+j] = 0;
                             field[(i+1)*WIDTH+(j+move)] = 2;
                             output_field(field);
-                            pausedelay(150000);
+                            //pausedelay(150000);
                         }
                     }
                 }
@@ -90,7 +148,7 @@ void figures(int *field) {
                             field[i*WIDTH+j] = 0;
                             field[(i+1)*WIDTH+(j+move)] = 2;
                             output_field(field);
-                            pausedelay(150000);
+                            //pausedelay(150000);
                         }
                     }
                 }
@@ -106,10 +164,12 @@ int check_update_filed(int *field, int *last_field) {
     for (int i = LENGTH-1; i >= 0; i--) { // 0 .. 49
         for (int j = WIDTH-1; j >= 0; j--) {
             if (last_field[i*WIDTH+j] != field[i*WIDTH+j]) {
+                //printf(" i = %d, j = %d, %d = %d\n", i, j, last_field[i*WIDTH+j], field[i*WIDTH+j]);
                 flag++;
             }
         }
     }
+    printf("%d \n", flag);
     return flag;
 }
 
